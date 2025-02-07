04bf3e666c1675d3872634cd80c17273ac7535d9
ologush <17ojl@queensu.ca>
2025-02-06 22:57:14 -0500

    Set up access point

--- code.py
+++ code.py
@@ -11,17 +11,9 @@ import socketpool
 
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
-print("Connecting to WiFi")
+print("Creating AP")
 
-wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
-
-print("Successfully connected to:" + " " + os.getenv('CIRCUITPY_WIFI_SSID'))
-
-pool = socketpool.SocketPool(wifi.radio)
-
-print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
-
-print("My IP address is", wifi.radio.ipv4_address)
+wifi.radio.start_ap(ssid=os.getenv('CIRCUITPY_AP_SSID'), password=os.getenv('CIRCUITPY_AP_PASSWORD'))
 
 while True:
     led.fill((255, 0, 0))
